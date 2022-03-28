8a4fedaeca8c49221f187f5fedd685166a254fc0
Priscilla Sebastine <priscilladanglang@gmail.com>
2022-03-28 09:46:14 +0100

    first commit

---
+++ ques1.py
@@ -0,0 +1,6 @@
+
+#let F be 3.1
+
+x = float(input((3.1*10)-15))
+
+print(x)
---
+++ ques2.py
@@ -0,0 +1,16 @@
+
+numbers = [34,3,17,546,1,207]
+
+print(max(numbers))
+
+#numbers.sort()
+#print(numbers)
+
+#print(numbers[5])
+
+#largest = 0
+#for i in numbers:
+    #if (i > largest):
+        #largest = i
+
+        #print(largest)
